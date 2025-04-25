a066dd11fe0eac6c9a1640d8ce5d650cfc10c836
Ancherm <andrewcherm2014@gmail.com>
2025-04-25 17:13:33 +0400

    create db

---
+++ server/database/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE "user"
+(
+    id            BIGSERIAL PRIMARY KEY,
+    username      VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255)        NOT NULL
+);
+
+CREATE TABLE receipt
+(
+    id      BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    data    OID    NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE product
+(
+    id         BIGSERIAL PRIMARY KEY,
+    user_id    BIGINT NOT NULL,
+    receipt_id BIGINT NOT NULL,
+    name       VARCHAR(255),
+    price      DECIMAL(10, 2),
+    category   VARCHAR(255),
+
+    FOREIGN KEY (receipt_id) REFERENCES receipt (id) ON DELETE CASCADE
+);
